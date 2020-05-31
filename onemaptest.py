0961aea65e9e9e52c060127cc117605757297ab1
namiwa <khairul.nasri895@gmail.com>
2020-05-31 18:17:09 +0800

    adding malls and mrt info

--- onemaptest.py
+++ onemaptest.py
@@ -12,7 +12,7 @@ def oneMapQuery(searchVal):
     returnGeom = 'Y'
     getAddrDetails = 'Y'
     pageNum = '1'
-    res = requests.get(oneMapApi + 'searchVal=' + searchVal + '&returnGeom=' + returnGeom + '&getAddrDetails=' + getAddrDetails + "&pageNum=" + pageNum);
+    res = requests.get(oneMapApi + 'searchVal=' + searchVal + '&returnGeom=' + returnGeom + '&getAddrDetails=' + getAddrDetails + "&pageNum=" + pageNum, verify=False);
     resJson = res.json();
     print(resJson)
     postal = resJson['results'][0]['POSTAL']
@@ -23,7 +23,7 @@ def oneMapQuery(searchVal):
     print("There was an error:", e)
     return ['' , '', '']
 
-# function for running query 
+# function for running query for main addresses
 def main():
   df = pd.read_csv('data/unique_addr.csv')
   store = []
@@ -42,6 +42,21 @@ def main():
   test.to_csv('data/test.csv')
 
 
+def mall_search():
+  df = pd.read_csv('data/shopping_malls_may_2020_sg.csv')
+  store = []
+  count = 0
+  for index, row in df.iterrows():
+    print(row)
+    count = count + 1
+    searchVal = row['Malls']
+    print(searchVal)
+    res = oneMapQuery(searchVal)
+    store.append(res)
+    time.sleep(1)
+  test = pd.DataFrame(store, columns=['postal', 'lat', 'long'])
+  df = df.join(test)
+  df.to_csv('data/test_malls.csv')
 
 if __name__ == '__main__':
-  main()
+  mall_search()
